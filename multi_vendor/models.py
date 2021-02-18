b3c6216a0437d963142942578e11ad4d0b175840
zizo-pixels <zizopixels11@gmail.com>
2021-02-18 18:27:34 +0500

    init

--- multi_vendor/models.py
+++ multi_vendor/models.py
@@ -11,7 +11,53 @@ from django.db import models
 
 
 
-# - vendors
+class Products(models.Model):
+    category = (
+        ('Sports', 'Sports'),
+        ('Business', 'Business'),
+        ('Computer', 'Computer'),
+        ('Entertainment', 'Entertainment'),
+        ('Games', 'Games'),
+        ('Family', 'Family'),
+        ('Travel', 'Travel'),
+        ('Fashion', 'Fashion'),
+        ('Politics', 'Politics'),
+        ('Pets', 'Pets'),
+    )
+    category = models.CharField(max_length=13, blank=True,null=True, choices= category)
+    title = models.CharField(max_length=100, null=True,blank=True)
+    content = models.CharField(max_length=1000, null=True,blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="blog_image")
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.user,'-blog_title-',self.title, '-image-', self.image)
+
+
+class Favorite(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {} {}'.format(self.user,'-fav-', self.blog)
+
+
+class Comments(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=250, null=True, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+
+
+
+
+# - Seller
    # - Name
    # - City
    # - Mob
