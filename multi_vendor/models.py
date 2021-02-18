e159e9eb7ce2bb39c11f512845d7676b4206c4c4
zizo-pixels <zizopixels11@gmail.com>
2021-02-18 17:46:10 +0500

    init

--- multi_vendor/models.py
+++ multi_vendor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# vendors
+ #  - Shop
+ #  - Products
+ #  - Orders
+ #  = Reviews
+ #  - Comments
+ #  - Social media links
+
+
+
+
+# users
+ # - Details
+#  - Save
+#  - Add to cart
