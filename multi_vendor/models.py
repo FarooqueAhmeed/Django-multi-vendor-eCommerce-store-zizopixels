ecf88b35585541d3a3bd780c97cf238d7a5e167d
zizo-pixels <zizopixels11@gmail.com>
2021-02-18 18:16:22 +0500

    init

--- multi_vendor/models.py
+++ multi_vendor/models.py
@@ -4,18 +4,90 @@ from django.db import models
 
 
 
-# vendors
- #  - Shop
- #  - Products
- #  - Orders
- #  = Reviews
- #  - Comments
- #  - Social media links
+# - vendors
+   # - Name
+   # - City
+   # - Mob
+   # - Address
+   # - Gmail
+   # - BIO
+   # - Image
+   # - DOB
+   # - Male/Female
+
+     # - Shop
+        # - Name
+        # - Logo
+        # - date of creation
+        # - BIO
+        # - venders(R)
+        # -
+
+
+
+     # - Products
+         # - Category
+         # - Name
+         # - Color
+         # - Size
+         # - Quantity
+         # - Description
+         # - Image
+         # - Prize
+         # - Date of added on shop
+         # - Shop(R)
+         # - Reviews(R)
+         # - Comments (R)
+         # - User (R)
+
+
+     # - Orders
+        # - Shop
+        # - Name (Products r)
+        # - User (R)
+        # - Quantity
+        # - color
+        # - size
+        # - Date
+
+
+     # = Reviews
+        # - User(R)
+        # - Product (R)
+        # - Shop(R)
+        # - Stars
+        # - Thoughts
+
+
+
+     # - Comments
+       # - Product(R)
+       # - User (R)
+       # - Comment
+
+    #  - Social media links
+       # - URLs
 
 
 
 
-# users
- # - Details
-#  - Save
-#  - Add to cart
+# - users
+    # - username
+    # - mail
+
+     # - Details
+         # - Name
+         # - Phone
+         # - Address
+         # - city
+         # - State
+         # - Country
+         # - Street
+         # - PostalCode
+         # - Room no
+         # - Building
+
+    # - Save
+       # - Product (R)
+    # - Add to cart
+       # - Product (R)
